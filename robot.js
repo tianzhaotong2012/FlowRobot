3dd142f59e2c70ba24346e65a19a0d987eb50c15
root <root@gzhxy-tianzhaotong.docker.iwm.name>
2018-06-27 09:29:37 +0800

    UA

--- robot.js
+++ robot.js
@@ -16,15 +16,16 @@ r1.on('line', (line) => {
 	//var execStr6 = 'timeout 600s node_modules/casperjs/bin/casperjs --proxy=' + line + ' --cookies-file=./cookies.txt casper6.js';
       	var execStr5 = 'timeout 600s ' + __dirname + '/node_modules/casperjs/bin/casperjs --proxy=' + line + ' --cookies-file=' + __dirname + '/cookies.txt ' + __dirname + '/casper5.js';
 	var execStr6 = 'timeout 600s ' + __dirname + '/node_modules/casperjs/bin/casperjs --proxy=' + line + ' --cookies-file=' + __dirname + '/cookies.txt ' + __dirname + '/casper6.js';
+	var execStr7 = 'timeout 600s ' + __dirname + '/node_modules/casperjs/bin/casperjs --proxy=' + line + ' --cookies-file=' + __dirname + '/cookies.txt ' + __dirname + '/casper7.js';
 	if(type == 5){
 		console.log("55555");
-		console.log(execStr5);
-		var data = exec(execStr5,{silent:true}).stdout;
+		console.log(execStr7);
+		var data = exec(execStr7,{silent:true}).stdout;
 	}
 	if(type == 6){
 		console.log("66666");
-		console.log(execStr6);
-		var data = exec(execStr6,{silent:true}).stdout;
+		console.log(execStr7);
+		var data = exec(execStr7,{silent:true}).stdout;
 	}
 	var data = exec(execStr,{silent:true}).stdout;
 	i += 1;

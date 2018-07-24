86466e53375fb656346d4f05ade2cb0e7ff7671a
root <root@gzhxy-tianzhaotong.docker.iwm.name>
2018-07-24 14:45:22 +0800

    add_key_word

--- casper7_v2.js
+++ casper7_v2.js
@@ -87,23 +87,58 @@ casper.userAgent(finalAgent.finalAgent);
 
 casper.options.viewportSize = {width: finalAgent.width, height: finalAgent.height};
 
+var curIP = getCurIp();
+console.log('curIP:'+JSON.stringify(curIP)); 
+var ilanguage = curIP.language; 
+console.log('language:'+ilanguage);  
+var acceptLanguage = curIP.acceptLanguage;
+var width = finalAgent.width;
+var height = finalAgent.height;
+
 casper.options.onPageInitialized = function(){
+  casper.page.customHeaders = {"Accept-Language": "en-US,en;q=0.9"};
 	//casper.page.injectJs("includes/util.js");
-  casper.page.evaluate(function() {
-        "window.screen = {width: " + finalAgent.width + ", height: " + finalAgent.height + "}";
-      }
-  );
-}
+  casper.page.evaluate(function(language,iwidth,iheight) {
+        //"window.screen = {width: " + finalAgent.width + ", height: " + finalAgent.height + "}";
+        //window.screen = {width: gWidth, height: gHeight};
+        navigator.systemLanguage = language
+        navigator.language = language
+        window.screen = {width: iwidth, height: iheight};
+        //window.screen = {width: finalAgent.width, height: finalAgent.height};
+      },ilanguage,width,height);
+};
 
 casper.start('http://m.baidu.com/');
 
+// casper.open('http://127.0.0.1/request.php',{
+//     headers: {
+//         'Accept-Language': 'en-US,en;q=0.9'
+//     }
+// }).then(function() {  
+// this.echo('GOT it.');  
+// });  
+
 casper.then(function() {
-   this.capture(__dirname + '/baidu-start.png');
+   this.capture(__dirname + '/baidu-start-v2.png');
 });
 
 casper.then(function() {
-   this.fill('form[id="index-form"]', { word: '做饭吃美食网' }, true);//填入form，进行搜索
+   var type = Math.floor(Math.random()*3+1);
+   if(type == 1){
+         this.fill('form[id="index-form"]', { word: '做饭吃美食网' }, true);//填入form，进行搜索
+   }
+   if(type == 2){
+          this.fill('form[id="index-form"]', { word: '在家烹饪' }, true);//填入form，进行搜索
+   }
+   if(type == 3){
+	  this.fill('form[id="index-form"]', { word: '做饭吃美食' }, true);
+   }
+   //this.fill('form[id="index-form"]', { word: '做饭吃美食网' }, true);
 });
+
+// casper.then(function() {
+//    this.fill('form[id="index-form"]', { word: '在家烹饪' }, true);//填入form，进行搜索
+// });
 
 casper.then(function() {  
   this.click('button[id="index-bn"]');  
@@ -114,7 +149,7 @@ casper.then(function() {
 }); 
 
 casper.then(function() {
-   this.capture(__dirname + '/baidu-search-results.png');
+   this.capture(__dirname + '/baidu-search-results-v2.png');
 });
 
 casper.then(function() {
@@ -129,7 +164,7 @@ casper.waitForSelector(x('//span[contains(text(),"www.zuofanchi.cn")]/../../../a
 			this.echo('Page title is: ' + this.evaluate(function() {
 				return window.screen.width;
 			}), 'INFO'); 
-			casper.capture(__dirname + '/ok.png');
+			casper.capture(__dirname + '/ok-v2.png');
   	   });  
 });
 
@@ -162,7 +197,7 @@ casper.waitForSelector('body > div.container > div > div.side-menu > div.col-md-
         casper.click('body > div.container > div > div.side-menu > div.col-md-5 > div > div:nth-child(3) > a');
     this.wait(6000,function() {  
           this.echo('search Successfully.'); 
-      casper.capture(__dirname + '/ok.png');
+      casper.capture(__dirname + '/ok-v2.png');
        });  
 });
 
@@ -182,7 +217,7 @@ casper.waitForSelector('body > div > div.row.padding > div.side-menu > div.col-m
         casper.click('body > div > div.row.padding > div.side-menu > div.col-md-5 > div.section-1 > div.row > div:nth-child(7) > a');
     this.wait(6000,function() {  
           this.echo('search Successfully.'); 
-      casper.capture(__dirname + '/ok.png');
+      casper.capture(__dirname + '/ok-v2.png');
        });  
 });
 
@@ -194,7 +229,7 @@ casper.waitForSelector('body > div > div.row.padding > div.side-menu > div.col-m
 
 
 casper.then(function() {
-   this.capture(__dirname + '/baidu-click-results.png');
+   this.capture(__dirname + '/baidu-click-results-v2.png');
 }); 
 
 casper.run();
